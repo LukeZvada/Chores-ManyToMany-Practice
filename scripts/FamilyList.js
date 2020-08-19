f5775cfda400715899a72d2a8aff4bf0b094131f
LukeZvada <zvadamusic@gmail.com>
2020-08-19 08:32:00 -0700

    first commit

---
+++ scripts/FamilyList.js
@@ -0,0 +1,80 @@
+import { useChores } from "./ChoreProvider.js"
+import { useFamilyMembers } from "./FamilyProvider.js"
+import { useFamilyChores } from "./FamilyChoreProvider.js"
+import { FamilyMember } from "./FamilyMember.js"
+
+const contentTarget = document.querySelector(".family")
+
+/*
+    Component state variables with initial values
+*/
+let chores = []
+let people = []
+let peopleChores = []
+
+
+/*
+    Main component logic function
+*/
+export const FamilyList = () => {
+    getChores()
+        .then(getFamilyMembers)
+        .then(getFamilyChores)
+        .then(() => {
+            chores = useChores()
+            people = useFamilyMembers()
+            peopleChores = useFamilyChores()
+
+            render()
+        })
+}
+
+/*
+    Component render function
+*/
+const render = () => {
+    contentTarget.innerHTML = people.map(person => {
+        const relationshipObjects = getChoreRelationships(person)
+        /*
+            End result for family member 1...
+
+            [
+                { "id": 1, "familyMemberId": 1, "choreId": 4 },
+                { "id": 2, "familyMemberId": 1, "choreId": 5 }
+            ]
+        */
+
+        const choreObjects = convertChoreIdsToChores(relationshipObjects)
+        /*
+            End result for family member 1...
+
+            [
+                { "id": 4, "task": "Clean the bedrooms" },
+                { "id": 5, "task": "Family game night" }
+            ]
+        */
+
+        // Get HTML representation of product
+        const html = FamilyMember(person, choreObjects)
+
+        return html
+    }).join("")
+}
+
+
+
+// Get corresponding relationship objects for a person
+const getChoreRelationships = (person) => {
+    const relatedChores = peopleChores.filter(pc => pc.familyMemberId === person.id)
+
+    return relatedChores
+}
+
+// Convert array of foreign keys to array of objects
+const convertChoreIdsToChores = (relationships) => {
+    const choreObjects = relationships.map(rc => {
+        return chores.find(chore => chore.id === rc.choreId)
+    })
+
+    return choreObjects
+}
